be414c097906eb812590f81dfac09c1014c77d89
Sleepytt1210 <54278461+Sleepytt1210@users.noreply.github.com>
2021-10-18 18:51:50 +0100

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,2 +1,4 @@
-document.getElementById("log").innerText = "Hello World"
+var divi = document.createElement("div");
+divi.innerText = "Hello World";
+document.getElementById("level6").append(divi)
 alert('Hello World')
